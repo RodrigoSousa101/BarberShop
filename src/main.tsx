3d1a38ff8d0dae304bbf1b74ec9eefc513e36bc5
Bacardi101 <a104241@uminho.pt>
2025-06-13 18:57:50 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from './Router.tsx';
 import ScrollToTop from './ScrollToTop'; // <--- importa o ScrollToTop aqui
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop /> {/* <--- adiciona aqui */}
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
